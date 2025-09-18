f72a650b3780c42a730bfeb383b5e50b09e89535
Chathura Dilshantha Kumara <chathura26322@gmail.com>
2025-09-18 14:55:24 +0530

    create, update and delete for product

--- src/main/java/com/codewithmosh/store/controller/ProductController.java
+++ src/main/java/com/codewithmosh/store/controller/ProductController.java
@@ -3,10 +3,12 @@ package com.codewithmosh.store.controller;
 import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.CategoryRepository;
 import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class  ProductController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping
     public Iterable<ProductDto>  getAllProducts(
@@ -38,4 +41,47 @@ public class  ProductController {
         return ResponseEntity.ok(productMapper.toProductDto(product));
     }
 
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto productRequest
+    ) {
+        var category = categoryRepository.findById(productRequest.getCategoryId()).orElse(null);
+        if(category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+
+       var product = productMapper.toEntity(productRequest);
+        product.setCategory(category);
+       productRepository.save(product);
+       productRequest.setId(product.getId());
+
+       return ResponseEntity.ok(productRequest);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto request, @PathVariable Long id) {
+        var category = categoryRepository.findById(request.getCategoryId()).orElse(null);
+        if(category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var product = productRepository.findById(id).orElse(null);
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        product.setCategory(category);
+        productMapper.update(request,product);
+        productRepository.save(product);
+        return ResponseEntity.ok(productMapper.toProductDto(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
@@ -4,9 +4,17 @@ import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(source = "category.id",target = "categoryId")
     ProductDto toProductDto(Product product);
+
+    Product toEntity(ProductDto productDto);
+
+    @Mapping(target = "id", ignore = true) // Ignore the id field during update
+    @Mapping(source = "categoryId",target = "category.id")
+    void update(ProductDto productDto, @MappingTarget Product product);
+
 }
